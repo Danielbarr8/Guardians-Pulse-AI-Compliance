c81e8f50be608ebc51a40135e8afee675ecd12a1
Daniel Barr <45768108+Danielbarr8@users.noreply.github.com>
2026-03-06 02:13:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://a023deae6c13e140b4d502d9db98303b@o4510993280139264.ingest.us.sentry.io/4510996074004480",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+)
 import streamlit as st
 from transformers import pipeline
 
